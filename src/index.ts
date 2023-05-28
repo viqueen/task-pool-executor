8607f2aeaec6af82642669481cd2349f1564a67a
viqueen <hasnae@viqueen.org>
2023-05-28 14:53:04 +1000

    LABSET-120: welcome eslint and license notice plugin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-import { CliProgressRunContext, TaskPoolExecutor } from './types';
-import { SimpleTaskPoolExecutor } from './simple-task-pool-executor';
+/**
+ * Copyright 2023 Hasnae Rehioui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { CliProgressTaskPoolExecutor } from './cli-progress-task-pool-executor';
+import { SimpleTaskPoolExecutor } from './simple-task-pool-executor';
+import { CliProgressRunContext, TaskPoolExecutor } from './types';
 
 export * from './types';
 
--- src/simple-task-pool-executor.ts
+++ src/simple-task-pool-executor.ts
@@ -1,6 +1,22 @@
-import EventEmitter from 'events';
-import { Runnable, TaskPoolExecutor } from './types';
+/**
+ * Copyright 2023 Hasnae Rehioui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { randomUUID } from 'crypto';
+import EventEmitter from 'events';
+
+import { Runnable, TaskPoolExecutor } from './types';
 
 export class SimpleTaskPoolExecutor<TOutput, TRunContext>
     extends EventEmitter
